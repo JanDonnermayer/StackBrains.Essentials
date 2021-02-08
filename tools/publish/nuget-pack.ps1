b7ee162177759b5b66c78571c384badc2b26bf0f
JanDonnermayer <jan@donnermayer.de>
2021-02-08 20:55:42 +0100

    use release config

--- tools/publish/nuget-pack.ps1
+++ tools/publish/nuget-pack.ps1
@@ -3,4 +3,4 @@ $root = git rev-parse --show-toplevel
 $nuspec = "$root/.nuget/StackBrains.Essentials.nuspec"
 $csproj = "$root/src/StackBrains.Essentials/StackBrains.Essentials.csproj"
 
-dotnet pack "$csproj" -p:NuspecFile="$nuspec"
+dotnet pack "$csproj" -p:NuspecFile="$nuspec" -c:Release

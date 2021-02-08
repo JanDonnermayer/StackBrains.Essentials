8038b36e354279d437920a813c00fb4e504eaf59
JanDonnermayer <jan@donnermayer.de>
2021-02-08 20:03:41 +0100

    add assets, config

--- src/StackBrains.Essentials/src/extensions/TaskSequenceExtensions.cs
+++ src/StackBrains.Essentials/src/extensions/TaskSequenceExtensions.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace System.Threading.Tasks
 {
-
     public static class TaskSequenceExtensions
     {
         /// <summary>

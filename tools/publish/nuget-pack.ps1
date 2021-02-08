e7ed2af5dd93a566489aca0c5d87c00635cc1916
JanDonnermayer <jan@donnermayer.de>
2021-02-08 22:48:39 +0100

    use project instead of nuspec

--- tools/publish/nuget-pack.ps1
+++
@@ -1,6 +0,0 @@
-$root = git rev-parse --show-toplevel
-
-$nuspec = "$root/.nuget/StackBrains.Essentials.nuspec"
-$csproj = "$root/src/StackBrains.Essentials/StackBrains.Essentials.csproj"
-
-dotnet pack "$csproj" -p:NuspecFile="$nuspec" -c:Release

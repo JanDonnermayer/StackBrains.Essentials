657194f6146ddc13c4161401926ca4e44e663211
JanDonnermayer <jan@donnermayer.de>
2021-06-24 11:29:12 +0200

    Add AnyAsync, FirstAsync.

--- src/StackBrains.Essentials/src/extensions/EnumerableExtensions.cs
+++ src/StackBrains.Essentials/src/extensions/EnumerableExtensions.cs
@@ -40,6 +40,35 @@ namespace System.Linq
             return state;
         }
 
+        public static async Task<bool> AnyAsync<T>(
+            this IEnumerable<T> source,
+            Func<T, Task<bool>> asyncPredicate
+        )
+        {
+            foreach (var task in source.Select(asyncPredicate))
+                if (await task) return true;
+
+            return false;
+        }
+
+        public static async Task<T?> FirstOrDefaultAsync<T>(
+            this IEnumerable<T> source,
+            Func<T, Task<bool>> asyncPredicate,
+            T? @default = default
+        )
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (asyncPredicate is null)
+                throw new ArgumentNullException(nameof(asyncPredicate));
+
+            foreach (var item in source)
+                if (await asyncPredicate(item)) return item;
+
+            return @default;
+        }
+
         /// <summary>
         /// Returns elements that are not null
         /// </summary>

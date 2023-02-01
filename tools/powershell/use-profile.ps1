1240df008f8d206ff48e7018e7475f768f6706e9
Jan Donnermayer <jan.donnermayer@googlemail.com>
2023-02-01 16:16:18 +0100

    cerate dir if not exists

--- tools/powershell/use-profile.ps1
+++ tools/powershell/use-profile.ps1
@@ -1,5 +1,6 @@
 
-$localProfile = "$env:USERPROFILE/documents/powershell/microsoft.powershell_profile.ps1"
+$localPowershellDir = "$env:USERPROFILE/documents/powershell"
+$localProfile = "$localPowershellDir/microsoft.powershell_profile.ps1"
 
 $addedProfile = Join-Path -Path (Get-Item $PSScriptRoot).FullName -ChildPath "init-profile.ps1"
 $initProfile = ". $addedProfile"
@@ -7,6 +8,10 @@ $initProfile = ". $addedProfile"
 function WriteProfile {
     Write-Host "Adding ""$addedProfile"" to ""$localProfile""."
     "$initProfile" >> $localProfile
+}
+
+if (!(Test-Path $localPowershellDir)) {
+    New-Item -ItemType Directory -Force -Path "$localPowershellDir"
 }
 
 if (!(Test-Path $localProfile)) {
